9719a39af9fc7eaef8ac321d5c2369346d8023f0
Patryk <maciejesiu2131@gmail.com>
2024-07-04 23:46:34 +0200

    Add Login and Logout functions

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app) # Database
 migrate = Migrate(app, db) # Database mirgration engine
-
+login = LoginManager(app)
+login.login_view = 'login'
+ 
 from app import routes, models
 
