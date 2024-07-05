f7b9851e880c26cc38dd1f2e6c177719a1cbe2bb
Patryk <maciejesiu2131@gmail.com>
2024-07-05 23:19:54 +0200

    Add user registration form with username, email and password with validation

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app import db
@@ -12,7 +12,6 @@ from urllib.parse import urlsplit
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'John'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -23,8 +22,7 @@ def index():
             'body': "It is to hot today"
         }
     ]
-    
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -55,5 +53,19 @@ def logout():
     return redirect(url_for('index'))
 
 
-        
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm(request.form)
+    if form.validate_on_submit():
+        user = User(username = form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Thanks for registering.')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
         
