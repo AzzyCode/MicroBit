ca8938498cc2b5af919c0084a576c8ad8c57bb87
Patryk <maciejesiu2131@gmail.com>
2024-08-03 23:22:35 +0200

    Fix problems with url

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, flash, redirect, url_for, request, g, current_app
+from flask import render_template, flash, redirect, url_for, request, g, current_app, g
 from flask_login import current_user, login_required
 from flask_babel import _, get_locale
+from app.main.forms import SearchForm
 import sqlalchemy as sa
 from app import db
 from app.main.forms import EditProfileForm, EmptyForm, PostForm
@@ -16,6 +17,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.now(timezone.utc)
         db.session.commit()
+        g.search_form = SearchForm()
     g.locale = str(get_locale())
 
 
@@ -64,13 +66,11 @@ def explore():
     )
     next_url = url_for("main.explore", page=posts.next_num) if posts.has_next else None
     prev_url = url_for("main.explore", page=posts.prev_num) if posts.has_prev else None
-    return render_template(
-        "index.html",
-        title=_("Explore"),
-        posts=posts.items,
-        next_url=next_url,
-        prev_url=prev_url,
-    )
+    return render_template( "index.html",
+                            title=_("Explore"),
+                            posts=posts.items,
+                            next_url=next_url,
+                            prev_url=prev_url,)
     
 
 @bp.route("/user/<username>")
@@ -158,3 +158,19 @@ def unfollow(username):
         return redirect(url_for("main.user", username=username))
     else:
         return redirect(url_for("main.index"))
+    
+
+@bp.route('/search')
+@login_required
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for('main.explore'))
+    page = request.args.get('page', 1, type=int)
+    posts, total = Post.search(g.search_form.q.data, page,
+                               current_app.config['POSTS_PER_PAGE'])
+    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
+        if total > page * current_app.config['POSTS_PER_PAGE'] else None
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
+        if page > 1 else None
+    return render_template('search.html', title=_('Search'), posts=posts,
+                           next_url=next_url, prev_url=prev_url)
